4ee930c266e136e70c9f8cc2ef82aa34d553e147
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-07-21 20:03:57 -0400

    fixing routes errors

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 //require dependencies
 const express = require('express');
+const router = express.Router();
 
 //create express app
 const app = express();
 
 //create a PORT variable
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 //set up express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-const apiRoutes = require("./routes/apiRoutes");
+const apiRoutes = require("./routes/apiRoutes"); 
 app.use(apiRoutes);
 const htmlRoutes = require("./routes/htmlRoutes");
 app.use(htmlRoutes);
